ea8f1f9ae780f2bfdd812079e1453bcce59339c8
Deepak Kumar Saw <aartideepak53@gmail.com>
2026-06-15 20:08:25 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://ecommerce-saas-backend.onrender.com/api",
+  // baseURL: "https://ecommerce-saas-backend.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
